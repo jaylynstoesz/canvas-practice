39f7eedec698dd0f8cb2f3b2d1ac96816de6b1e1
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-15 16:12:59 -0600

    refactored

--- app.js
+++ app.js
@@ -1,68 +1,50 @@
 window.onload = function() {
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById('canvas');
 var pickColor = document.getElementById('pickColor');
 var pickSize = document.getElementById('pickSize');
 var pickShape = document.getElementById('pickShape');
-var eraseAll = document.getElementById('eraseAll');
-// var collection = [];
-var squares = [];
-var circles = [];
-var rectangles = [];
+var square = [];
+var circle = [];
+var rectangle = [];
 var ctx = canvas.getContext('2d');
 
-function Shape() {
+function Shape(x, y, wr, color, hst) {
+  this.x = x;
+  this.y = y;
+  this.wr = wr;
+  this.hst = hst;
+  this.color = color;
 }
 
-function Rectangle(x, y, w, h, color) {
-  Shape.call(this);
+function Rectangle(x, y, wr, hst, color) {
+  Shape.call(this, x, y, wr, color, hst);
   this.type = "rectangle";
-  this.color = color;
-  this.sides = 4;
-  this.x = x;
-  this.y = y;
-  this.wr = w;
-  this.hst = h;
 }
-
-function Square(x, y, w, color) {
-  Shape.call(this);
-  this.type = "square";
-  this.sides = 4;
-  this.color = color;
-  this.x = x;
-  this.y = y;
-  this.wr = w;
-  this.hst = w;
-}
-
-function Circle(x, y, r, st, color) {
-  Shape.call(this);
-  this.type = "circle";
-  this.color = color;
-  this.x = x;
-  this.y = y;
-  this.wr = r;
-  this.hst = st;
-}
-
 Rectangle.prototype = new Shape();
+
+function Square(x, y, wr, color) {
+  Shape.call(this, x, y, wr, color);
+  this.type = "square";
+  this.hst = wr;
+}
 Square.prototype = new Shape();
+
+function Circle(x, y, wr, hst, color) {
+  Shape.call(this, x, y, wr, color, hst);
+  this.type = "circle";
+}
 Circle.prototype = new Shape();
 
 Shape.prototype.drawRect = function() {
-  if (canvas.getContext) {
-    // var ctx = canvas.getContext("2d");
-    ctx.fillStyle = this.color;
-    ctx.fillRect (this.x, this.y, this.wr, this.hst);
-  }
+  ctx.fillRect (this.x, this.y, this.wr, this.hst);
+  ctx.fillStyle = this.color;
 };
 
 Shape.prototype.drawCircle = function() {
-    // var ctx = canvas.getContext('2d');
-    var path=new Path2D();
-    path.arc(this.x, this.y, this.wr, this.hst, Math.PI*2, true);
-    ctx.fillStyle = this.color;
-    ctx.fill(path);
+  var path=new Path2D();
+  path.arc(this.x, this.y, this.wr, this.hst, Math.PI*2, true);
+  ctx.fill(path);
+  ctx.fillStyle = this.color;
 };
 
 Shape.prototype.collect = function(x, y, wr, hst, color) {
@@ -74,98 +56,93 @@ Shape.prototype.collect = function(x, y, wr, hst, color) {
   item.wr = this.wr;
   item.hst = this.hst;
   if (item.type === "square") {
-    squares.push(item);
-    console.log(squares);
+    square.push(item);
+    console.log(square);
   } else if (item.type === "rectangle") {
-    rectangles.push(item);
-    console.log(rectangles);
+    rectangle.push(item);
+    console.log(rectangle);
   } else if (item.type === "circle") {
-    circles.push(item);
-    console.log(circles);
+    circle.push(item);
+    console.log(circle);
   }
 };
 
-var rectbtn = document.getElementById('rectbtn');
-rectbtn.onclick = function() {
+function create(x) {
   var width = Math.floor(Math.random()*canvas.width);
   var height = Math.floor(Math.random()*canvas.height);
-  var newRect = new Rectangle(width, height, 100 * pickSize.value, 50 * pickSize.value, pickColor.value);
-  newRect.drawRect();
-  newRect.collect();
-};
-
-var squbtn = document.getElementById('squbtn');
-squbtn.onclick = function(e) {
-  var width = Math.floor(Math.random()*canvas.width);
-  var height = Math.floor(Math.random()*canvas.height);
-  var newSquare = new Square(width, height, 100 * pickSize.value, pickColor.value);
-  newSquare.drawRect();
-  newSquare.collect();
-};
-
-var cirbtn = document.getElementById('cirbtn');
-cirbtn.onclick = function() {
-  var width = Math.floor(Math.random()*canvas.width);
-  var height = Math.floor(Math.random()*canvas.height);
-  var newCircle = new Circle(width, height, 50 * pickSize.value, 0, pickColor.value);
-  newCircle.drawCircle();
-  newCircle.collect();
-};
+  var newItem;
+  if (x === "rectangle") {
+    newItem = new Rectangle(width, height, 100 * pickSize.value, 50 * pickSize.value, pickColor.value);
+    newItem.drawRect();
+  } else if (x === "circle") {
+    newItem = new Circle(width, height, 50 * pickSize.value, 0, pickColor.value);
+    newItem.drawCircle();
+  } else if (x === "square") {
+    newItem = new Square(width, height, 100 * pickSize.value, pickColor.value);
+    newItem.drawRect();
+  }
+  newItem.collect();
+}
 
 canvas.addEventListener('click', function(e) {
   var x = e.offsetX;
   var y = e.offsetY;
+  var newItem;
   if (pickShape.value === "Circle") {
-    var newCircle = new Circle(x, y, 50 * pickSize.value, 0, pickColor.value);
-    newCircle.drawCircle();
-    newCircle.collect();
-  } else if (pickShape.value === "Square") {
-    var newSquare = new Square(x, y, 100 * pickSize.value, pickColor.value);
-    newSquare.drawRect();
-    newSquare.collect();
-  } else if (pickShape.value === "Rectangle") {
-    var newRect = new Rectangle(x, y, 100 * pickSize.value,  50 * pickSize.value, pickColor.value);
-    newRect.drawRect();
-    newRect.collect();
+    newItem = new Circle(x, y, 50 * pickSize.value, 0, pickColor.value);
+    newItem.drawCircle();
+    } else {
+      if (pickShape.value === "Square") {
+      newItem = new Square(x, y, 100 * pickSize.value, pickColor.value);
+      } else if (pickShape.value === "Rectangle") {
+      newItem = new Rectangle(x, y, 100 * pickSize.value, 50 * pickSize.value, pickColor.value);
+    }
+    newItem.drawRect();
   }
+  newItem.collect();
 });
 
-eraseRects.onclick = function() {
-  for (var i = 0; i < rectangles.length; i++) {
-    var type = rectangles[i].type;
-    var x = rectangles[i].x;
-    var y = rectangles[i].y;
-    var wr = rectangles[i].wr;
-    var hst = rectangles[i].hst;
-    if (type === "rectangle") {
-        console.log(rectangles[i]);
-        ctx.clearRect(x,y,wr,hst);
-      }
-    }
-  rectangles = [];
-  console.log(rectangles);
-  console.log(circles);
-};
-
-eraseCir.onclick = function() {
-  console.log(circles.length);
-  for (var i = 0; i < circles.length; i++) {
-    var type = circles[i].type;
-    var x = circles[i].x;
-    var y = circles[i].y;
-    var wr = circles[i].wr;
-    var hst = circles[i].hst;
-    if (type === "circle") {
-      console.log(circles[i]);
+function erase(array) {
+  for (var i = 0; i < array.length; i++) {
+    var type = array[i].type;
+    var x = array[i].x;
+    var y = array[i].y;
+    var wr = array[i].wr;
+    var hst = array[i].hst;
+    if (array === circle) {
       ctx.beginPath();
       ctx.arc(x, y, wr, hst, Math.PI*2, true);
       ctx.clearRect(x - wr - 1, y - wr - 1, wr * 2 + 2, wr * 2 + 2);
+      } else {
+      ctx.clearRect(x,y,wr,hst);
+      }
     }
-  }
-  circles = [];
-  console.log(circles);
-  console.log(rectangles);
+  array = [];
+}
+
+rectbtn.onclick = function() {
+  create("rectangle");
 };
-      //
+
+squbtn.onclick = function() {
+  create("square");
+};
+
+cirbtn.onclick = function() {
+  create("circle");
+};
+
+eraseRects.onclick = function() {
+  erase(rectangle);
+};
+
+eraseCir.onclick = function() {
+  erase(circle);
+};
+
+eraseSq.onclick = function() {
+  erase(square);
+};
+
 // Closing window bracket
 };
